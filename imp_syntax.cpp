d4adf3e0ddfc2872712b4ae31062844e7230e0a1
adarsh-iiit <adarshtech251@gmail.com>
2025-04-07 15:55:14 +0530

    first commit

---
+++ imp_syntax.cpp
@@ -0,0 +1,8 @@
+#include<utility> ===========>>>> for pairs 
+pair<int , int > pair_name = {1,2} ;
+cout << pair_name.first << "   " << pair_name.second ; 
+
+
+
+vector< pair<int , int > > nameofvec = {   {1,2}  , {3,4}   .........    }
+vector<  vector<int>  > mat ( no_of_rows ,   vector<int>(number_of_col ,  by_default_value ) )
